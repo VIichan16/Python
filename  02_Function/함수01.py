96b53781b73ba1af78fc1e4afec17e6734ebbbc1
Sungnam <126052997+VIichan16@users.noreply.github.com>
2024-10-26 18:23:44 +0900

    FastCampus Python
    2024-10-26

---
+++  02_Function/함수01.py
@@ -0,0 +1,21 @@
+""""
+세개의 정수를 인자로 받아, 합계와 평균을 출력해주는 함수이다.
+함수를 호출한 결과로 표준 출력이 나오도록 정의해보자.
+
+함수 호출
+printSumAvg(10,20,30)
+
+표준출력)
+합계 60 평균 20
+
+"""
+
+def printSumAvg(num1,num2,num3):
+    avg = total/3
+
+    print(f"합계 {total} 평균 {int(avg)}")
+
+
+
+printSumAvg(10,20,30)
+
